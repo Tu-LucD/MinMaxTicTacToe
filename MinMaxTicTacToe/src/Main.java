da0169eec4ba82d4fc6c53cfc5ef256fcd8fe93a
Tu-LucD <tlduong123@gmail.com>
2023-03-08 10:54:12 -0500

    Finished user input and gameplay

--- MinMaxTicTacToe/src/Main.java
+++ MinMaxTicTacToe/src/Main.java
@@ -1,16 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Board b = new Board();
-        char result = b.play('O',0,0);
-        System.out.println(result);
-        b.printBoard();
-
-        result = b.play('O',1,1);
-        System.out.println(result);
-        b.printBoard();
-
-        result = b.play('O',2,2);
-        System.out.println(result);
-        b.printBoard();
+        TicTacToe ttt = new TicTacToe();
     }
 }
--- MinMaxTicTacToe/src/TicTacToe.java
+++ MinMaxTicTacToe/src/TicTacToe.java
@@ -1,4 +1,37 @@
+import java.util.Scanner;
+
 public class TicTacToe {
     private int turn;
     private Board board;
+
+    public TicTacToe(){
+        Scanner sc = new Scanner(System.in);
+        board = new Board();
+        char gameState;
+        do{
+//            Player Move
+            if(turn % 2 == 0){
+                System.out.println("Player's turn");
+                System.out.println("What row?");
+                int row = sc.nextInt();
+                System.out.println("What column?");
+                int col = sc.nextInt();
+                gameState = board.play('X',row,col);
+            }
+//            //Bot Move
+            else{
+            System.out.println("Bot's turn");
+                gameState = board.play('O',0,0);
+            }
+            board.printBoard();
+            System.out.println("----------------");
+            turn++;
+        }while (gameState == 'N');
+        if(gameState != 'T'){
+            System.out.println("THE WINNER IS "+gameState);
+        }
+        else{
+            System.out.println("THE GAME IS A TIE");
+        }
+    }
 }
